a5f4964480822226e3f238868e5899205f77c118
Subhamoy Sarkar <subhamoysarkar526@gmail.com>
2023-12-19 18:31:17 +0530

    Announcement page dynamic

--- app/src/App.js
+++ app/src/App.js
@@ -4,6 +4,7 @@ import Dashboard from "./pages/Dashboard";
 import Appbar from './components/Appbar';
 import Profile from "./components/Profile";
 import SideBar from "./components/Sidebar/SideBar";
+import Announcement from "./pages/Announcement";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './css/App.css';
 function App() {
@@ -16,6 +17,7 @@ function App() {
             <Route path='/' element={<Dashboard/>}></Route>
             <Route path="/Profile" element={<Profile />} />
             <Route path="/Profile/profile" element={<Profile />} />
+            <Route path="/Announcement" element={<Announcement/>}/>
             {/* 404 not found */}
             <Route path="*" element={<> not found</>} />
           </Routes>
