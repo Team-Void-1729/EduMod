9d72dd308b1efca0d1c66b84423a65a2e2081630
Subhamoy Sarkar <subhamoysarkar526@gmail.com>
2023-12-19 15:22:33 +0530

    changes

--- app/src/App.js
+++ app/src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import Dashboard from "./pages/Dashboard";
 import Appbar from './components/Appbar';
-import Settings from "./pages/Settings";
 import Profile from "./components/Profile";
 import SideBar from "./components/Sidebar/SideBar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
